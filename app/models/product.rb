1b596e8d9f65d8f4929e9cf35f4c8bb15945d759
Mark Wetzel <mark@markwetzel.com>
2013-10-29 20:46:40 -0400

    Nightly.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+  
+  has_many  :line_items
+  before_destroy  :ensure_not_referenced_by_any_line_item
+  
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
@@ -6,4 +10,17 @@ class Product < ActiveRecord::Base
     with: /\.(gif|jpg|png)\z/i,
     message: "must be a URL for GIF, PNG, or JPG image."
   }
+  
+  private
+  
+  def ensure_not_referenced_by_any_line_item
+    if lines.empty?
+      true
+    else
+      errors.add(:base, 'Line Items Present')
+      false
+    end
+  end
+  
+  
 end
